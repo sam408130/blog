c0a6c09fa574a3e3bbecd3ba1ddd655352109ba1
root <root@debian>
2015-11-19 13:43:45 +0900

    uwsgi deploy

---
+++ my_blog/wsgi.py
@@ -0,0 +1,8 @@
+import os,sys
+
+if not os.path.dirname(__file__) in sys.path[:1]:
+    sys.path.insert(0, os.path.dirname(__file__))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+from django.core.handlers.wsgi import WSGIHandler
+application = WSGIHandler()
